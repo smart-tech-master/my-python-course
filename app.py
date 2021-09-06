493e6cebf9c99a55fdfb93d1e82a5a65c553aaf8
Preston <kpreston313@gmail.com>
2021-09-07 02:40:01 +1000

    define class and using del

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-countries_file = open('./new.py','w');
-countries_file.write('print(\'This is new file\')');
+class Person:
+    def __init__(self, name, age):
+        self.name = name;
+        self.age = age;
+
+p1 = Person('Jhon', 18);
+del p1.name
+print(p1.name);
