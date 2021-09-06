3b7f85891bde7c9045c86fc4dfdff5ff307df3cc
Preston <kpreston313@gmail.com>
2021-09-07 01:48:04 +1000

    try, except, finally

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ try:
     x = int(input('Please input an integer: '));
     print(x);
 except:
-    print('something went wrong')
+    print('something went wrong');
+finally:
+    print('completed all')
