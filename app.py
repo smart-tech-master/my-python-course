2301a907cd70501541d0dffa8b39521d8cf7e193
Preston <kpreston313@gmail.com>
2021-09-06 14:57:41 +1000

    list method

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-list1 = [1,2,3,4];
-list2 = ['Aplle', 'Banana', 'Mango', 'orange'];
+list1 = [3,4,2,1];
+list2 = ['Apple', 'Banana', 'Mango', 'orange'];
+list3 = list2.copy();
 
-list1.extend(list2);
-print(list1);
+# list1.extend(list2);
+# list2.append('cherry');
+# list2.insert(8, 'cherry');
+# list2.remove('Banana');
+# list2.clear();
+# index = list2.index('Mango');
+# index = list2.count('Mango');
+# print(list2);
+# list1.sort();
+# list2.reverse();
+# list2.pop();
+# list2.pop(2);
+del list2
+print(list2);
